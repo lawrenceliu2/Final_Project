29eebd5e46bd558592edb0d00fa440d718f9f252
Lawrence Liu <lawrence255@yahoo.com>
2017-01-10 13:09:39 -0500

    stuff done

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, hashlib
 
 #Checks if username and password exist
 def UserAuth(username, password):
@@ -16,11 +16,26 @@ def UserAuth(username, password):
 
 #Checks if username is taken
 def nameAvail(username):
-    db = sqlite3.connect("data/memebase.db")
+    db = sqlite3.connect("data/dbsm.db")
     users = db.cursor()
 
     q = "SELECT * FROM users WHERE username = \"%s\";" % (username)
     users.execute(q)
     info = users.fetchall()
-    if (len(info) > 0): return False
-    else: return True
+    if (len(info) > 0):
+        return False
+    return True
+
+def addUser(username, password):
+    db = sqlite3.connect("data/dbsm.db")
+    users = db.cursor()
+    m = hashlib.sha1(password).hexdigest()
+ 
+    if nameAvail(username):
+        q = '''INSERT INTO users(username, password, wins, gamesPlayed) VALUES("%s", "%s", "%s", %s)''' % (username, m, 0, 0)
+        users.execute(q)
+        db.commit()
+        return "User added"
+    return "User not added"
+
+print addUser("lawrence", "puggo")

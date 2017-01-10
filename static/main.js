dac6c68573774cd12259692882286e22f0af7016
Julian Atkin <jatkin@stuy.edu>
2017-01-09 21:23:05 -0500

    fixed mouse behavior off canvas

--- static/main.js
+++ static/main.js
@@ -13,25 +13,6 @@ var SocketMgr = {
 var Canvas = {
     canv: null,
     ctx: null,
-    /*buf: (function() {
-	var items = [];
-	return {
-	    enqueue:  function(data) {
-		items.push(data);
-	    },
-	    dequeue: function() {
-		var temp = items[0];
-		items.splice(1);
-		return temp;
-	    },
-	    peek: function() {
-		return items[0];
-	    },
-	    size: function() {
-		return items.length;
-	    }
-	};
-    }()),*/
     init: function() {
 	this.canv = document.getElementById("canv");
 	this.canv.width = screen.width;
@@ -52,36 +33,30 @@ var Canvas = {
       	    Canvas.ctx.beginPath();
       	    Canvas.ctx.moveTo(e.clientX,e.clientY);
       	    Canvas.canv.addEventListener("mousemove",fx);
+	    Canvas.canv.addEventListener("mouseoff",function() {
+		Canvas.ctx.stroke();
+		Canvas.ctx.closePath();
+	    });
+	    Canvas.canv.addEventListener("mouseover",function(e) {
+		Canvas.ctx.beginPath();
+		Canvas.ctx.moveTo(e.clientX,e.clientY);
+	    });
       	});
       	Canvas.canv.addEventListener("mouseup",function() {
-	    	//Canvas.draw.isDrawing = false;
-	    	//SocketMgr.socket.emit("up");
       		Canvas.canv.removeEventListener("mousemove",fx);
       		Canvas.ctx.closePath();
       	});
     },
     draw: function(data) {
-	//this.isDrawing = false;
 	if (!(data.isDrawing)) {
 	    Canvas.ctx.closePath();
 	    Canvas.ctx.beginPath();
-	    Canvas.ctx.moveTo(data[0].x,data[0].y);//Canvas.buf.peek().x,Canvas.buf.peek().y);
+	    Canvas.ctx.moveTo(data[0].x,data[0].y);
 	}
 	else {
 	    Canvas.ctx.lineTo(data.x,data.y);
       	    Canvas.ctx.stroke();
 	}
-	/*for (var i=0; i<Canvas.buf.length; i++) {
-	    var data = Canvas.buf[i];
-	    Canvas.ctx.lineTo(data.x,data.y);
-      	    Canvas.ctx.stroke();
-	}*/
-	/*while (data.length > 0) {
-	    //var data = Canvas.buf.dequeue();
-	    Canvas.ctx.lineTo(data.x,data.y);
-      	    Canvas.ctx.stroke();
-	    data.splice(1);
-	}*/
     },
 };
 
